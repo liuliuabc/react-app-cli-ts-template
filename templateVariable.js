717aabf7518f17694df37f6987edf7320aeb42ae
lyh <lyh@YNSH100367.local>
2019-12-26 12:45:11 +0800

    modify name

--- templateVariable.js
+++ templateVariable.js
@@ -2,5 +2,5 @@ module.exports=[{
     type: 'input',
     message: '请输入项目名字',
     name: 'productName',
-    default: "eact-app-cli-ts-template demo" // 默认值
+    default: "react-app-cli-ts-template demo" // 默认值
 }]
