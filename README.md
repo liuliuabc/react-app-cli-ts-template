11ae3b21bf30baca579aa8d10ba933d8c27f1854
lyh <lyh@YNSH100367.local>
2019-12-26 11:20:18 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# 经营诊断报告(kuhe-permission-control)
+
+## 项目技术
+该项目基于`create-react-app`创建,并通过`react-app-rewired`来完成不执行`eject`也能修改webpack相关配置
+* react
+* antd
+* mobx
+* typescript
+
+## 项目部署与调试
+* 开发版调试 `yarn start:develop`
+
+* 打包staging版 `yarn build:staging`
+
+* 打包prod版 `yarn build:prod`
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+import './App.css';
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                恭喜你，<react-app-cli-projectName>项目已经成功创建
+            </div>
+        );
+    }
+}
+export default App;
